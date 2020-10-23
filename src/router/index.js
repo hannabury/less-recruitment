14e4eaa16c61570b44fdee5534ac036852269c38
Hanna Bury <hanna.o.bury@gmail.com>
2020-10-23 18:28:16 +0200

    Simple 404 view

--- src/router/index.js
+++ src/router/index.js
@@ -3,10 +3,18 @@ import VueRouter from "vue-router";
 import Coins from "../views/Coins/Index.vue";
 import CoinsList from "../views/Coins/List.vue";
 import CoinsDetails from "../views/Coins/Details.vue";
+import Error404 from "../views/404.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
+  {
+    path: '/',
+    name: 'home',
+    redirect: {
+      name: 'coins'
+    }
+  },
   {
     path: "/coins",
     name: "coins",
@@ -26,7 +34,8 @@ const routes = [
         component: CoinsDetails,
       },
     ],
-  }
+  },
+  { path: '*', component: Error404 }
 ];
 
 const router = new VueRouter({
