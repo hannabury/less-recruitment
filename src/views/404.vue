8433f96ef7ee00e7428d93694af9f4541bf78cb8
Hanna Bury <hanna.o.bury@gmail.com>
2020-10-23 18:52:56 +0200

    Simple 404 view

--- src/views/404.vue
+++ src/views/404.vue
@@ -1,9 +1,15 @@
 <template>
   <div class="container">
-    <h1 class="heading">404 error</h1>  
+    <h1 class="heading_h1">404 error</h1>  
   </div>
 </template>
 
 <script>
 export default {}
 </script>
+
+<style lang="scss" scoped>
+ .heading_h1 {
+   text-align: center;
+ }
+</style>
